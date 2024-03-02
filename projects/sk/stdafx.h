c9030acae0f09e8c521b9789522adcc4408beafa
memade <k34ubll5@outlook.com>
2024-03-03 04:24:34 +0800

    updated.

---
+++ projects/sk/stdafx.h
@@ -0,0 +1,6 @@
+#ifndef __PROJECTS_SK_STDAFX_H_
+#define __PROJECTS_SK_STDAFX_H_
+
+#include <sk.h>
+
+#endif ///__PROJECTS_SK_STDAFX_H_
--- tests/test_sk/main.cc
+++ tests/test_sk/main.cc
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <sk.h>
 
 int main(int argc, char *argv[]) {
+  char *zip_compress_data = nullptr;
+  size_t zip_compress_data_len = 0;
+  sk_zip_compress("", 0, &zip_compress_data, &zip_compress_data_len);
   std::cout << "Hello SK !" << std::endl;
+
   return 0;
 }
