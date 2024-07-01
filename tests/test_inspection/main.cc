7ae75cecb00bc85e5d9f6d4ff45f489aba16e4cb
memade <k34ubll5@outlook.com>
2024-07-01 16:03:48 +0800

    updated

--- tests/test_inspection/main.cc
+++ tests/test_inspection/main.cc
@@ -1,29 +1,14 @@
 #include "stdafx.h"
 
-inspect::Inspection *__gpInspect = nullptr;
-uiohook::IUiohook *__gpUiohook = nullptr;
 int main(int argc, char *argv[]) {
-#ifdef _WIN32
-  __gpInspect = inspect::Inspection::Create("inspection.dll");
-  __gpUiohook = uiohook::IUiohook::Create("uiohook.dll");
-#else
-  __gpInspect = inspect::Inspection::Create("inspection.so");
-#endif
-
-  //__gpInspect->Start();
-  __gpUiohook->Start();
 
   std::string input;
   do {
     input.clear();
     std::getline(std::cin, input);
     if (input == "q" || std::cin.eof()) {
-      //__gpInspect->Stop();
-      __gpUiohook->Stop();
       break;
     }
   } while (1);
-  // inspect::Inspection::Destroy(&__gpInspect);
-  uiohook::IUiohook::Destroy(&__gpUiohook);
   return 0;
 }
--- tests/test_inspection/stdafx.h
+++ tests/test_inspection/stdafx.h
@@ -1,6 +1,8 @@
-#ifndef __TEST_WX_AFX_H_
+#ifndef __TEST_INSPECTION_AFX_H_
 #define __TEST_WX_AFX_H_
 #include <stl.h>
+#include <fmt/format.h>
 #include <inspection.h>
 #include <libuiohook.h>
-#endif ///__TEST_WX_AFX_H_
+
+#endif ///__TEST_INSPECTION_AFX_H_
